3887ae1409691bf8b54bf7b2f56ddd7bfed674ce
shaya9-ali <shayan543212@gmail.com>
2026-03-27 11:05:58 +0500

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   allowedDevOrigins: ["27.96.93.124"],
 };
 
 export default nextConfig;
-=======
-  /* config options here */
-};
-
-export default nextConfig;
->>>>>>> b7c7ea63851aefeb00e32bf037964ec6794c2e19
